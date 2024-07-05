56df7829b830a22bdb916b9913298efb8d5322cf
24Skynet24 <ksk836684@gmail.com>
2024-07-06 03:03:56 +0400

    add types

---
+++ src/types/index.ts
@@ -0,0 +1,24 @@
+export interface rating {
+    rate: string,
+    subtitle: string,
+}
+
+export interface feedback {
+    userImg: string,
+    userName: string,
+    userLink: string,
+    userTxt: string
+}
+
+export interface storeLink {
+    img: string,
+    subTitle: string,
+    title: string,
+    link: string,
+}
+
+export interface coin {
+    img: string,
+    name: string,
+    alt: string
+}
