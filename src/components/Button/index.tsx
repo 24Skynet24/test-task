89514f2a340531014b3dbef14c9637b224c56975
24Skynet24 <ksk836684@gmail.com>
2024-07-05 20:34:17 +0400

    add Button component

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,11 @@
+import "./button.scss"
+
+const ButtonGradient = (props) => {
+    return (
+        <button {...props} className="button_gr">
+            <span>Join Whatsapp</span>
+        </button>
+    )
+}
+
+export default ButtonGradient
